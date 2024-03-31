818bc50f023d796d292a7fcf872822441744dbdc
Navodya Weerasooriya <58646038+Navodya99@users.noreply.github.com>
2024-03-31 19:39:32 +0530

    Splash Screen

---
+++ Views/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  ZigZag
+//
+//  Created by Navodya Weerasooriya on 2024-03-31.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
