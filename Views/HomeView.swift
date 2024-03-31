60217277595df084c6066b95fbccc37b9a913599
Navodya Weerasooriya <58646038+Navodya99@users.noreply.github.com>
2024-03-31 20:14:18 +0530

    Home View added

---
+++ Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  ZigZag
+//
+//  Created by Navodya Weerasooriya on 2024-03-31.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
