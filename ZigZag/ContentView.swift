bf9b83a499f910af511fc9fe338ccdecfdda5fd3
Navodya Weerasooriya <58646038+Navodya99@users.noreply.github.com>
2024-03-31 14:45:48 +0530

    Initial Commit

---
+++ ZigZag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZigZag
+//
+//  Created by Navodya Weerasooriya on 2024-03-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZigZag/ZigZagApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZigZagApp.swift
+//  ZigZag
+//
+//  Created by Navodya Weerasooriya on 2024-03-31.
+//
+
+import SwiftUI
+
+@main
+struct ZigZagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
