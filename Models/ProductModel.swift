fb2aa3e667c10c1be86124b02ff4cc2884592342
Navodya Weerasooriya <58646038+Navodya99@users.noreply.github.com>
2024-04-02 03:39:26 +0530

    Added model and sample data

---
+++ Models/ProductModel.swift
@@ -0,0 +1,18 @@
+//
+//  ProductModel.swift
+//  ZigZag
+//
+//  Created by Navodya Weerasooriya on 2024-04-02.
+//
+
+import SwiftUI
+
+struct ProductModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductModel()
+}
