395e31fe33919b473ebe9e463e7f3fc131df1192
Navodya Weerasooriya <58646038+Navodya99@users.noreply.github.com>
2024-04-02 20:30:37 +0530

    Detail View Added

--- View/DetailView.swift
+++ View/DetailView.swift
@@ -9,7 +9,61 @@ import SwiftUI
 
 struct DetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            VStack(alignment: .leading){
+                ZStack{
+                    Image("f1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                HStack{
+                    Spacer()
+                    Text("SIZE M")
+                        .padding(.trailing,20)
+                        .foregroundStyle(Color.white)
+                        .fontWeight(.semibold)
+                        .frame(height: 36)
+                        .padding(.horizontal)
+                        .background(Color("SplashBlue"))
+                        .cornerRadius(20)
+                        .padding(.top, -28)
+                        .padding(.trailing,-20)
+
+                    
+                }
+                
+                Text("Product Name Product Name Product Name ")
+                    .font(.title)
+                    .padding(.horizontal,10)
+                Text("Product Description Product Description Product Description Product Description Product Description Product Description ")
+                    .font(.system(size: 15))
+                    .padding(.horizontal,10)
+                    .opacity(0.7)
+                
+                
+                
+                   
+                
+                
+                
+            }
+            Spacer()
+            VStack{
+                HStack{
+                    Text("ADD TO CART")
+                        .fontWeight(.medium)
+                    Spacer()
+                    Text("LKR 7878")
+                        .fontWeight(.medium)
+                        .opacity(0.8)
+                }
+                .padding()
+                .foregroundColor(.white)
+                .background(Color("AppBlack"))
+                .cornerRadius(5)
+                .padding()
+            }
+        }
     }
 }
 
