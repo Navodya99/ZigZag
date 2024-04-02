a09a1a3eb7471c98e69968e1c1ee4a5705956af1
Navodya Weerasooriya <58646038+Navodya99@users.noreply.github.com>
2024-04-02 20:30:00 +0530

    Detail View Added

---
+++ View/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  ZigZag
+//
+//  Created by Navodya Weerasooriya on 2024-04-02.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
