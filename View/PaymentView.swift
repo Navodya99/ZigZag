81ef9525a82e74986458651cf0addaf4ed6cde6e
Navodya Weerasooriya <58646038+Navodya99@users.noreply.github.com>
2024-04-07 23:59:52 +0530

    Payment View

---
+++ View/PaymentView.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentView.swift
+//  ZigZag
+//
+//  Created by Navodya Weerasooriya on 2024-04-07.
+//
+
+import SwiftUI
+
+struct PaymentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaymentView()
+}
