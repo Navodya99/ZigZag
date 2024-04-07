b844f3cc30c31c6b5495ab9e4893879980ee245d
Navodya Weerasooriya <58646038+Navodya99@users.noreply.github.com>
2024-04-08 00:18:18 +0530

    Minor changes

--- View/DetailView.swift
+++ View/DetailView.swift
@@ -9,60 +9,58 @@ import SwiftUI
 
 struct DetailView: View {
     let item: ProductModel
+    @EnvironmentObject var cartViewModel: CartViewModel
+    @State private var isNavigationActive = false
+    
     var body: some View {
-        VStack{
-            VStack(alignment: .leading){
-                ZStack{
-                    Image(item.imageName)
-                        .resizable()
-                        .scaledToFit()
-                }
-                HStack{
-                    Spacer()
-                    Text("SIZE \(item.size)")
-                        .padding(.trailing,20)
-                        .foregroundStyle(Color.white)
-                        .fontWeight(.semibold)
-                        .frame(height: 36)
-                        .padding(.horizontal)
-                        .background(Color("SplashBlue"))
-                        .cornerRadius(20)
-                        .padding(.top, -28)
-                        .padding(.trailing,-20)
-
-                    
-                }
-                
-                Text(item.itemName)
-                    .font(.title)
-                    .padding(.horizontal,10)
-                Text(item.description)
-                    .font(.system(size: 15))
-                    .padding(.horizontal,10)
-                    .opacity(0.7)
-                
-                
-                
-                   
-                
-                
-                
-            }
-            Spacer()
+        NavigationStack{
             VStack{
-                HStack{
-                    Text("ADD TO CART")
-                        .fontWeight(.medium)
-                    Spacer()
-                    Text("LKR \(String(format: "%.2f", item.price))")
-                        .fontWeight(.medium)
-                        .opacity(0.8)
+                VStack(alignment: .leading){
+                    ZStack{
+                        Image(item.imageName)
+                            .resizable()
+                            .scaledToFit()
+                    }
+                    HStack{
+                        Spacer()
+                        Text("SIZE \(item.size)")
+                            .padding(.trailing,20)
+                            .foregroundStyle(Color.white)
+                            .fontWeight(.semibold)
+                            .frame(height: 36)
+                            .padding(.horizontal)
+                            .background(Color("SplashBlue"))
+                            .cornerRadius(20)
+                            .padding(.top, -28)
+                            .padding(.trailing,-20)
+                    }
+                    
+                    Text(item.itemName)
+                        .font(.title)
+                        .padding(.horizontal,10)
+                    Text(item.description)
+                        .font(.system(size: 15))
+                        .padding(.horizontal,10)
+                        .opacity(0.7)
                 }
-                .padding()
-                .foregroundColor(.white)
-                .background(Color("AppBlack"))
-                .cornerRadius(5)
-                .padding()
+                Spacer()
+                Button(action: {
+                    cartViewModel.addToCart(item: item)
+                }) {
+                    HStack {
+                        Text("ADD TO CART")
+                            .fontWeight(.medium)
+                        Spacer()
+                        Text("LKR \(String(format: "%.2f", item.price))")
+                            .fontWeight(.medium)
+                            .opacity(0.8)
+                    }
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color("AppBlack"))
+                    .cornerRadius(5)
+                    .padding()
+                }
             }
         }
     }
