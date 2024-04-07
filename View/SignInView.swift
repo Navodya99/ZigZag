b73e9f6a111f4b9c62cbe79f4f683045d587efd3
Navodya Weerasooriya <58646038+Navodya99@users.noreply.github.com>
2024-04-07 22:31:43 +0530

    Login and Register Screens

---
+++ View/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  ZigZag
+//
+//  Created by Navodya Weerasooriya on 2024-04-07.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
