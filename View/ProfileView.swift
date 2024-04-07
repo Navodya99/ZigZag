2b15a0b77064610dba82424405b986f703373b65
Navodya Weerasooriya <58646038+Navodya99@users.noreply.github.com>
2024-04-08 00:06:53 +0530

    Update UI

---
+++ View/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  ZigZag
+//
+//  Created by Navodya Weerasooriya on 2024-04-08.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
