6992bf2a1da0ca80f47c7f0c8672877c9827660d
Navodya Weerasooriya <58646038+Navodya99@users.noreply.github.com>
2024-04-07 22:32:50 +0530

    Login and Register Screens

--- View/RegisterView.swift
+++ View/RegisterView.swift
@@ -8,10 +8,121 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State private var userId : String = ""
+    @State private var password : String = ""
+    @State private var navigateSign : Bool =  false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            ZStack{
+                Spacer()
+                
+            
+                .hidden()
+                
+                NavigationLink(destination: RegisterView(), isActive: $navigateSign) {
+                    EmptyView()
+                }
+                .hidden()
+                
+                VStack{
+                    Spacer()
+                    VStack{
+                        Text("zigzag.")
+                            .font(.system(size: 60, weight: .semibold))
+                            .foregroundColor(.white)
+                        Text("Women's clothing store")
+                            .font(.system(size: 15, weight: .medium))
+                            .foregroundColor(.white)
+                            .opacity(0.7)
+                        
+                        
+                    }
+                    .padding(.top)
+                    VStack{
+                        TextField("Username or Email", text: $password)
+                            .foregroundColor(.black)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                        
+                        TextField("Phone", text: $password)
+                            .foregroundColor(.black)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                        
+                        
+                        SecureField("Password", text: $userId)
+                            .foregroundColor(.black)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                        
+                        HStack{
+                            Spacer()
+                            Text("Register Now")
+                                .foregroundColor(.white)
+                                .fontWeight(.semibold)
+                                .font(.system(size: 18))
+                            Spacer()
+                        }
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.white, lineWidth: 2)
+                        )
+                        .onTapGesture {
+                            navigateSign = true
+                            userId =  ""
+                            password = ""
+                        }
+                        
+                        
+                        HStack{
+                            Spacer()
+                            Text("Don't have zigzag account ?")
+                                .foregroundColor(.white)
+                                .fontWeight(.medium)
+                                .font(.system(size: 16))
+                                .opacity(0.7)
+                            Text("Register")
+                                .underline()
+                                .foregroundColor(.white)
+                                .fontWeight(.semibold)
+                                .font(.system(size: 16))
+                                .onTapGesture {
+                                    navigateSign = true
+                                }
+                            Spacer()
+                        }
+                        .padding(.top,10)
+                        
+                        
+                        
+                    }
+                    .padding(.horizontal)
+                    .padding(.top)
+                    
+                    
+                    Spacer()
+                    
+                }
+            }
+            .frame(maxWidth: .infinity , maxHeight: .infinity)
+            .background(Color("SplashBlue"))
+            .ignoresSafeArea()
+            
+        }
+        .navigationBarBackButtonHidden(true)
+
     }
+    
+    
 }
+
+    
+
 
 #Preview {
     RegisterView()
--- View/SignInView.swift
+++ View/SignInView.swift
@@ -8,10 +8,115 @@
 import SwiftUI
 
 struct SignInView: View {
+    @State private var userId : String = ""
+    @State private var password : String = ""
+    @State private var navigateHome : Bool =  false
+    @State private var navigateRegister : Bool =  false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            ZStack{
+                Spacer()
+                
+                NavigationLink(destination: HomeView(), isActive: $navigateHome) {
+                    EmptyView()
+                }
+                .hidden()
+                
+                NavigationLink(destination: RegisterView(), isActive: $navigateRegister) {
+                    EmptyView()
+                }
+                .hidden()
+                
+                VStack{
+                    Spacer()
+                    VStack{
+                        Text("zigzag.")
+                            .font(.system(size: 60, weight: .semibold))
+                            .foregroundColor(.white)
+                        Text("Women's clothing store")
+                            .font(.system(size: 15, weight: .medium))
+                            .foregroundColor(.white)
+                            .opacity(0.7)
+                        
+                        
+                    }
+                    .padding(.top)
+                    VStack{
+                        TextField("Username or Email", text: $password)
+                            .foregroundColor(.black)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                        
+                        
+                        SecureField("Password", text: $userId)
+                            .foregroundColor(.black)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                        
+                        HStack{
+                            Spacer()
+                            Text("Sign In")
+                                .foregroundColor(.white)
+                                .fontWeight(.semibold)
+                                .font(.system(size: 18))
+                            Spacer()
+                        }
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.white, lineWidth: 2)
+                        )
+                        .onTapGesture {
+                            navigateHome = true
+                            userId =  ""
+                            password = ""
+                        }
+                        
+                        
+                        HStack{
+                            Spacer()
+                            Text("Have an Account")
+                                .foregroundColor(.white)
+                                .fontWeight(.medium)
+                                .font(.system(size: 16))
+                                .opacity(0.7)
+                            Text("Sign in")
+                                .underline()
+                                .foregroundColor(.white)
+                                .fontWeight(.semibold)
+                                .font(.system(size: 16))
+                                .onTapGesture {
+                                    navigateRegister = true
+                                }
+                            Spacer()
+                        }
+                        .padding(.top,10)
+                        
+                        
+                        
+                    }
+                    .padding(.horizontal)
+                    .padding(.top)
+                    
+                    
+                    Spacer()
+                    
+                }
+            }
+            .frame(maxWidth: .infinity , maxHeight: .infinity)
+            .background(Color("SplashBlue"))
+            .ignoresSafeArea()
+            
+        }
+        .navigationBarBackButtonHidden(true)
     }
+    
 }
+
+
 
 #Preview {
     SignInView()
