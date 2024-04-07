c66e2217ab3c346d445298c326ecef972db7e0f0
Navodya Weerasooriya <58646038+Navodya99@users.noreply.github.com>
2024-04-08 00:00:12 +0530

    Payment View

--- View/PaymentView.swift
+++ View/PaymentView.swift
@@ -8,11 +8,61 @@
 import SwiftUI
 
 struct PaymentView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    let total: Double
+    @State private var name: String = ""
+    @State private var address: String = ""
+    @State private var showAlert = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack {
+                Text("Total to Pay")
+                    .padding(.top)
+                Text("LKR \(String(format: "%.2f", total)) ")
+                    .fontWeight(.semibold)
+                    .font(.system(size: 40))
+
+                TextField("Name", text: $name)
+                    .foregroundColor(.black)
+                    .padding()
+                    .background(Color.black.opacity(0.05))
+                    .cornerRadius(10)
+                TextField("Address", text: $address)
+                    .foregroundColor(.black)
+                    .padding()
+                    .background(Color.black.opacity(0.05))
+                    .cornerRadius(10)
+            }
+            .padding()
+            
+            Spacer()
+            
+            Button("Pay") {
+                showAlert = true
+            }
+            .foregroundColor(.white)
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color("SplashBlue"))
+            .cornerRadius(30)
+            .fontWeight(.bold)
+            .frame(height: 80)
+            .padding()
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Payment Successful"), message: Text("Thank you for your purchase!"), dismissButton: .default(Text("OK")) {
+                 
+                    presentationMode.wrappedValue.dismiss()
+                })
+            }
+        }
+        .navigationTitle("Payment")
     }
 }
 
+
+
 #Preview {
-    PaymentView()
+    PaymentView(total: 1000.00)
 }
